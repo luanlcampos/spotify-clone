8dc101d38248c32b2f269915d6d479c3d1643268
luanlcampos <luanlcampos15@gmail.com>
2023-05-04 22:05:50 -0700

    change page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import MainContent from "@/components/MainContent";
 import NavBar from "@/components/NavBar";
 import Player from "@/components/Player";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="grid-container h-full max-h-screen gap-x-4 pt-2">
-      <NavBar />
-      <MainContent />
-      <div className="footer">
-        <Player />
+    <>
+      <Head>
+        <title>Spotify Clone - Casion</title>
+      </Head>
+      <div className="grid-container h-full max-h-screen gap-x-4 pt-2">
+        <NavBar />
+        <MainContent />
+        <div className="footer">
+          <Player />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
