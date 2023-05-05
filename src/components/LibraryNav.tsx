4c6b7bfc04435816f2e644ced65e0351d495dace
luanlcampos <luanlcampos15@gmail.com>
2023-05-04 22:02:15 -0700

    fix nav height

--- src/components/LibraryNav.tsx
+++ src/components/LibraryNav.tsx
@@ -5,7 +5,7 @@ import LibraryNavHeader from "./LibraryNavHeader";
 
 const LibraryNav = () => {
   return (
-    <div className="library-nav flex flex-col gap-y-3 px-3 py-2 bg-[var(--foreground-color)] rounded-lg h-[calc(100%-80px)]">
+    <div className="library-nav flex flex-col gap-y-3 px-3 py-2 bg-[var(--foreground-color)] rounded-lg h-[calc(100%-120px)]">
       <LibraryNavHeader />
       <LibraryNavChips />
       <LibraryNavBody />
