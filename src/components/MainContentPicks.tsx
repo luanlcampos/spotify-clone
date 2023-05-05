b25a3170018774a11e2971623014f9e5b763beb9
luanlcampos <luanlcampos15@gmail.com>
2023-05-05 09:40:52 -0700

    fix card size and add some responsiveness

--- src/components/MainContentPicks.tsx
+++ src/components/MainContentPicks.tsx
@@ -2,17 +2,17 @@ const MainContentPicks = () => {
   return (
     <div className="picks-container w-full pr-3">
       <h1 className="text-3xl font-semibold mb-3">Good Afternoon</h1>
-      <div className="picks w-full grid grid-cols-3 grid-rows-2 gap-y-3 gap-x-5 ">
+      <div className="picks w-full grid grid-cols-2 grid-rows-3 xl:grid-cols-3 xl:grid-rows-2 gap-y-3 gap-x-5 ">
         {[...Array(6)].map((e, i) => {
           return (
             <div
-              className="pick-card flex items-center bg-white bg-opacity-25 rounded-md gap-x-3 hover:cursor-pointer hover:bg-opacity-40"
+              className="pick-card flex items-center h-16 lg:h-20 bg-white bg-opacity-30 rounded-sm gap-x-3 hover:cursor-pointer hover:bg-opacity-40 font-semibold"
               key={i}
             >
               <img
                 src={`http://via.placeholder.com/50x50`}
                 alt="img"
-                className="rounded-l-md"
+                className="rounded-l-sm h-full "
               />
               <span>Lorem Ipsum</span>
             </div>
